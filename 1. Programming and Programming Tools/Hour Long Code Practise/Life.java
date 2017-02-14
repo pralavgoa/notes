d622ce707b61cbddf98e5097ad361076d2dae558
Pralav Dessai <pralavgoa@gmail.com>
2017-02-14 13:33:40 -0800

    work laptop

--- 1. Programming and Programming Tools/Hour Long Code Practise/Life.java
+++ 1. Programming and Programming Tools/Hour Long Code Practise/Life.java
@@ -2,9 +2,16 @@
 * Codify my Life
 *
 */
+
+import java.util.Map;
+import java.util.HashMap;
+
 class Life{
+	private static final String TODAY = "Monday";
 	private static enum DAY_OF_WEEK {Monday,Tuesday,Wednesday,Thursday,Friday};
 	private static final String[] BIRTHDAY = {"1955","01","01"};
+	private static final Map<String,Boolean> IS_WEEKEND= new HashMap<>();
+	
 	public static void main(String[] args){
 		System.out.println("This is my life");
 		//Read the day of week from the args, else ask the user for the day of week
@@ -20,6 +27,20 @@ class Life{
 			}
 			
 		}
+		checkIfWeekend(TODAY);
+	}
 
+
+	public static void checkIfWeekend(String today){
+		IS_WEEKEND.put("Saturday",true);
+		IS_WEEKEND.put("Sunday",true);
+
+		if(IS_WEEKEND.containsKey(TODAY)){
+			if(IS_WEEKEND.get(TODAY)){
+				System.out.println("Yay today is weekend");
+			}
+		}else{
+			System.out.println("Today is a weekday");
+		}
 	}
 }
--- 1. Programming and Programming Tools/Hour Long Code Practise/Life.py
+++ 1. Programming and Programming Tools/Hour Long Code Practise/Life.py
@@ -1,6 +1,7 @@
 class Life:
 	@staticmethod
 	def run():
+		TODAY="Monday"
 		DAYS = ("Mon","Tue","Wed","Thu","Fri")
 		BIRTHDAY = ("1955","01","01")
 		print("This is my life")
@@ -13,5 +14,15 @@ class Life:
 				print("Month is "+ BIRTHDAY[i])
 			elif i==2:
 				print("Day is "+ BIRTHDAY[i])
+		Life.checkIfWeekend(TODAY)
+
+	@staticmethod
+	def checkIfWeekend(today):
+		IS_WEEKEND = {"Saturday":True, "Sunday":True}
+		if today in IS_WEEKEND:
+			if IS_WEEKEND[today] == True:
+				print("Today is a weekend")
+		else:
+			print("Today is a weekday")
 
 Life.run()
